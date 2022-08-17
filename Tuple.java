113cb0fca6b3acbc2052a25add3d170a911e8757
rsura <rahul.s.sura@gmail.com>
2022-08-16 20:15:35 -0700

    Created Tuple and Tuple Example classes

---
+++ Tuple.java
@@ -0,0 +1,11 @@
+/**
+ * This Tuple class is meant to be similar to the Python 
+ * data structure, tuple. It keeps the limitations,
+ * functionality, and style of Java in mind while implementing 
+ * as much of Python as possible
+ * 
+ * @author  Rahul Sura
+ */
+public class Tuple{
+    
+}
---
+++ TupleEx.java
@@ -0,0 +1,15 @@
+/**
+ * This class is to demonstrate some examples 
+ * and use cases of my Java Tuple implementation.
+ * 
+ * @author  Rahul Sura
+ */
+public class TupleEx {
+    /**
+     * Main method to test the Java Tuple.
+     * @param   args [UNUSED]
+     */
+    public static void main(String[] args) {
+        
+    }
+}
